5f271b32785a5c53ab79499e7469f2d71dc9d382
chacham <chacham@linecorp.com>
2020-04-28 00:45:40 +0900

    Add gRPC dependencies and implement GreeterGrpcService

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,30 @@
+import com.google.protobuf.gradle.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.12")
+    }
+}
+
 plugins {
+    id("idea")
     id("org.springframework.boot") version "2.2.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version "1.3.71"
     kotlin("plugin.spring") version "1.3.71"
+    id("com.google.protobuf") version "0.8.12"
 }
 
 group = "life.yalk.server"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+val grpcStarterVersion = "3.5.3"
+val protobufVersion = "3.11.4"
+val grpcVersion = "1.29.0"
 
 val developmentOnly by configurations.creating
 configurations {
@@ -26,6 +41,11 @@ repositories {
 }
 
 dependencies {
+    implementation("io.github.lognet:grpc-spring-boot-starter:$grpcStarterVersion")
+    implementation("com.google.protobuf:protobuf-java:$protobufVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
@@ -48,6 +68,24 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "11"
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                id("grpc")
+            }
+        }
     }
 }
---
+++ src/main/kotlin/life/yalk/server/quokka/grpc/GreeterGrpcService.kt
@@ -0,0 +1,17 @@
+package life.yalk.server.quokka.grpc
+
+import io.grpc.stub.StreamObserver
+import life.yalk.grpc.GreeterGrpc
+import life.yalk.grpc.HelloReply
+import life.yalk.grpc.HelloRequest
+import org.lognet.springboot.grpc.GRpcService
+
+@GRpcService
+class GreeterGrpcService : GreeterGrpc.GreeterImplBase() {
+    override fun sayHello(request: HelloRequest?, responseObserver: StreamObserver<HelloReply>?) {
+        println("Requested with :" + request?.name);
+        val reply = HelloReply.newBuilder().setMessage("Hi, " + request?.name).build()
+        responseObserver?.onNext(reply)
+        responseObserver?.onCompleted()
+    }
+}
