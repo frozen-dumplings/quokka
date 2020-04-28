cd8afdf1b4a8d311fc2fa98b5dc004672a028448
chacham <chacham@linecorp.com>
2020-04-28 21:46:52 +0900

    Add null check for GreeterGrpc

--- src/main/kotlin/life/yalk/server/quokka/grpc/GreeterGrpcService.kt
+++ src/main/kotlin/life/yalk/server/quokka/grpc/GreeterGrpcService.kt
@@ -9,9 +9,12 @@ import org.lognet.springboot.grpc.GRpcService
 @GRpcService
 class GreeterGrpcService : GreeterGrpc.GreeterImplBase() {
     override fun sayHello(request: HelloRequest?, responseObserver: StreamObserver<HelloReply>?) {
-        println("Requested with :" + request?.name);
-        val reply = HelloReply.newBuilder().setMessage("Hi, " + request?.name).build()
-        responseObserver?.onNext(reply)
-        responseObserver?.onCompleted()
+        if (request == null || responseObserver == null) {
+            return
+        }
+        println("Requested with :" + request.name);
+        val reply = HelloReply.newBuilder().setMessage("Hi, " + request.name).build()
+        responseObserver.onNext(reply)
+        responseObserver.onCompleted()
     }
 }
