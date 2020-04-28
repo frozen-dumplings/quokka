bf034dd07bb44ca82cd3205101a338a727f056e1
chacham <chacham@linecorp.com>
2020-04-28 22:08:53 +0900

    Change User/Message class to data class

--- src/main/kotlin/life/yalk/server/quokka/model/Message.kt
+++ src/main/kotlin/life/yalk/server/quokka/model/Message.kt
@@ -1,9 +1,5 @@
 package life.yalk.server.quokka.model
 
-class Message(id: Long?, sender: Long, receiver: Long) {
-    val id: Long? = id
-    val sender: Long = sender
-    val receiver: Long = receiver
-
-    constructor(sender: Long, receiver: Long): this(null, sender, receiver)
+data class Message(val id: Long?, val sender: Long, val receiver: Long) {
+    constructor(sender: Long, receiver: Long) : this(null, sender, receiver)
 }
